689593c1b8a44498ec8ffedd4dfa6629c0ea1d36
nahyunji <racoonto@gmail.com>
2021-05-11 20:05:05 +0900

    머드게임

---
+++ StudyCode02/Player.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCode02
+{
+    internal class Player
+    {
+        public string userName;
+        public int power;
+        public int hp;
+        private int maxHp = 10;
+
+        public Player(string _userName, int power, int mapHp)
+        {
+            userName = _userName;
+            this.power = power;
+            maxHp = mapHp;
+            this.hp = maxHp;
+        }
+
+        internal void RestoreHp()
+        {
+            if (hp < maxHp)
+                hp++;
+        }
+    }
+}
